fdb9bef705c81253416151c36ee75e456bc3f843
Daniel Rust <dlrust@gmail.com>
2025-05-28 07:40:54 -0700

    Initial version 0.1

--- devhub/cli.py
+++ devhub/cli.py
@@ -1,21 +1,21 @@
 import click
+from dotenv import load_dotenv
+import os
+
+from .commands.theme import theme
+
+if 'WORKING_DIR' not in os.environ:
+    # auto-set the working directory
+    os.environ['WORKING_DIR'] = os.getcwd()
+
+dotenv_path = os.path.abspath(
+    os.path.join(os.environ['WORKING_DIR'], '.env'))
+load_dotenv(dotenv_path=dotenv_path)
 
 
 @click.group()
-@click.version_option()
+@click.version_option(package_name='devhub-cli', prog_name='devhub-cli')
 def cli():
     "CLI interface to devhub"
 
-
-@cli.command(name="command")
-@click.argument(
-    "example"
-)
-@click.option(
-    "-o",
-    "--option",
-    help="An example option",
-)
-def first_command(example, option):
-    "Command description goes here"
-    click.echo("Here is some output")
+cli.add_command(theme)
